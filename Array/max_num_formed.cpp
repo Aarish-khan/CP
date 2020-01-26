e57236eeda84ea27dceeb9761a1f906a6b884e01
Aarish-khan <59867538+Aarish-khan@users.noreply.github.com>
2020-01-26 17:57:29 +0530

    find maximum number formed from array elements
    given an array of integers. how should we arrange the elements so that number formed is maximum. (question from gfg)

---
+++ Array/max_num_formed.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+using namespace std;
+
+bool decide(int a, int b)
+{
+    int temp1 = stoi(to_string(a)+to_string(b));
+    int temp2 = stoi(to_string(b)+to_string(a));
+    
+    if(temp1>=temp2)
+    return false;
+    else
+    return true;
+}
+
+
+string greatest(int *arr, int n)
+{
+    for(int i=0; i<n; i++)
+    {
+        int maxindex = i;
+        for(int j=i+1; j<n;j++)
+        {
+            
+            if(decide(arr[maxindex],arr[j]))
+            maxindex = j;
+            
+        }
+        swap(arr[maxindex],arr[i]);
+    }
+    string str = "";
+    for(int i =0; i<n; i++)
+    {
+        str += to_string(arr[i]);
+    }
+    
+    return str;
+    
+}
+
+
+int main() {
+	//code
+	int t; //no. of testcases;
+	cin>>t;
+	while(t--)
+	{
+	    int n;
+	    cin>>n;
+	    int arr[n];
+	    for(int i=0; i<n; i++)
+	    {
+	        cin>>arr[i];
+	    }
+	    
+	    cout<<greatest(arr, n)<<endl;
+	}
+	return 0;
+}
