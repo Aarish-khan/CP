cf985f4632d04d002a72e67da7467b0deac05aee
Aarish <0787mohdaarish@gmail.com>
2020-08-30 09:45:20 +0530

    Sum of leaf nodes of a binary tree

---
+++ Tree/011.SumofLeafNodes.cpp
@@ -0,0 +1,11 @@
+//https://practice.geeksforgeeks.org/problems/sum-of-leaf-nodes/1/
+
+int sumLeaf(Node* root)
+{
+    // Code here
+    if(root==nullptr) return 0 ;
+    
+    if(root->left == nullptr && root->right == nullptr) return root->data;
+    
+    return sumLeaf(root->left) + sumLeaf(root->right);
+}
